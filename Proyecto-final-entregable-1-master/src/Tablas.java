653381d7f5a2ca10bf44692bcf8cfa5d10625201
skamelot <luis_130295@hotmail.com>
2018-03-01 23:36:00 -0600

    Tablas
    ineditable ya

---
+++ Proyecto-final-entregable-1-master/src/Tablas.java
@@ -0,0 +1,82 @@
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class Tablas {
+	private final static String []ENCABEZADO_FILAS = new String[] {" ","1","2","3","4", "5", "6", "7", "8", "9","10","11","12","13","14","15"};
+	private final static String []ENCABEZADO_COLUMNAS = new String[] {" ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"};
+	private final static String []ENCABEZADO_SELECCION = new String[] {"Color", "Nombre del terreno", "ID terreno en .txt"};
+	
+	private final static int FILAS = 15;
+	private final static int COLUMNAS = 16;
+	
+	private JTable tabla;
+	private DefaultTableModel modeloTabla;
+	
+	
+	Tablas(String tipoTabla){
+		if(tipoTabla!="Terrenos") {
+			System.out.println("si entra");
+			//Celdas NO editables, para poder seleccionar el mapa y preguntar datos de las casillas sin que se editen
+			modeloTabla = new DefaultTableModel() {
+				private static final long serialVersionUID = 1L;
+				@Override
+			    public boolean isCellEditable(int row, int column) {
+					return false;
+					
+					//Con esto la columna ID no puede ser editable pero las demás sí
+			      }
+			};
+		}else {
+			System.out.println("no entra");
+			modeloTabla = new DefaultTableModel() {
+				private static final long serialVersionUID = 1L;
+				boolean[] editables = new boolean[]{
+	                    true, true, false
+	            };
+	            
+				@Override
+			    public boolean isCellEditable(int row, int column) {
+					if(column ==2)
+					{
+						System.out.println("if 1");
+						return  false;
+						
+					}
+					else 
+					{
+						System.out.println("if 2");
+						return  true;
+						
+					}//Con esto la columna ID no puede ser editable pero las demás sí
+			      }
+				
+			};
+			
+			modeloTabla.setColumnIdentifiers(ENCABEZADO_SELECCION);
+		}
+		
+		tabla = new JTable(modeloTabla);
+	    tabla.setFillsViewportHeight(true);
+	    tabla.setRowSelectionAllowed(true);
+	}//Fin constructor
+	
+	private void tablaVacia() {
+		modeloTabla = (DefaultTableModel) tabla.getModel();
+		modeloTabla.setRowCount(0);
+	}
+	
+	public JTable actualizarTabla(String tipoTabla, String[] datos) {
+		tablaVacia();
+		
+		if(tipoTabla.equals("Terrenos")) {
+			for(int i=0; i<datos.length; i++)
+				modeloTabla.addRow(new Object[]{" ", " ", datos[i]});
+		}
+		
+		return tabla;
+	}
+	
+	public JTable muestraTabla() { //Inventario completo de total o escasos 
+		return tabla;
+	}
+}
